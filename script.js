76f4136386b0732924b3f5678083a58bee3c1f4a
Yuri_Gomes <gomesyuridasilva@gmail.com>
2024-05-14 17:20:44 -0300

    feat: carrosel em andamento

--- script.js
+++ script.js
@@ -24,3 +24,43 @@ onresize = () => {
     xis.style.display = "inline"
     barras.style.display = "none"
 }
+
+// função carrossel
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+let numSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximo = () => {
+    banner.classList.remove(slides[slideAtual])
+    if (slideAtual < numSlides - 1){
+        slideAtual++
+    }
+    else{
+        slideAtual= 0
+    }
+    banner.classList.add(slides[slideAtual])
+
+   
+}
+
+const mostrarAnterior = () => {
+    banner.classList.remove(slides[slideAtual])
+    if (slideAtual > 0){
+        slideAtual--
+    }
+    else{
+        slideAtual= numSlides - 1
+    }
+    banner.classList.add(slides[slideAtual])
+
+}
